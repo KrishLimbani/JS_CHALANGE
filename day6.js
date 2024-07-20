0d87d6283acc134f01e448679866c507580b0fc1
KrishLimbani <160731235+KrishLimbani@users.noreply.github.com>
2024-07-20 20:04:57 +0530

    add file

--- day6.js
+++ day6.js
@@ -10,3 +10,21 @@ arr.pop()
 arr.shift()
 arr.unshift(1)
 console.log(arr)
+
+// use a for loop to print each element of an Array
+
+let each = [1,2,3,4,5,6,7,8,9,10]
+
+console.log("USING FOR LOOP")
+
+for(let i = 0; i <= each.length; i++){
+    console.log(each[i])
+}
+
+// use for each loop to print each element of an Array
+
+each.forEach(function(element) {
+    console.log(element)
+});
+
+
